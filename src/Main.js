6a561fe91b35b0e2ca9596204ac1c156020852fa
밍씽 <mingssing@sstaar.local>
2022-02-03 18:14:44 +0900

    [ADD] 초기 환경 구축

---
+++ src/Main.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export const Main = () => {
+  return <div>하잇</div>;
+};
---
+++ src/Router.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+export default function Router() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Main />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
