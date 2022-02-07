1ecb0d81669451a8d312b913af1940acd8dc0c56
KIM_star <78401083+sstaar91@users.noreply.github.com>
2022-02-07 19:00:46 +0900

    [ADD] menu hover에 따른 컨텐츠 변경 (#2)
    * [ADD] 1차 페이지 세팅
    
    * [ADD] 메뉴 hover에 따른 데이터 값 보여주기 (29th 기만님 review)
    
    Co-authored-by: 밍씽 <mingssing@sstaar.local>

--- src/Pages/first/First.js
+++ src/Pages/first/First.js
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { MenuTab } from '../../components/menuTab/MenuTab';
 
 export const First = () => {
   return (
     <section>
-      <div>1차 프로젝트 기준으로 기능 구현</div>
-      <div>먼저 모달창 구현</div>
+      <h1>1차 프로젝트 기준으로 기능 구현</h1>
+      <MenuTab /
     </section>
   );
 };
