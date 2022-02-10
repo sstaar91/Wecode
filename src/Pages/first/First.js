ba9b8be9a90aac778dd78faaecaa8f4a641c510e
sstaar91 <sstaar91@gmail.com>
2022-02-10 11:12:24 +0900

    [ADD] 이미지 슬라이드 구현

--- src/Pages/first/First.js
+++ src/Pages/first/First.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { MenuTab } from '../../components/menuTab/MenuTab';
 import { SelectBox } from '../../components/selectBox/SelectBox';
+import { ImgSlide } from '../../components/imgSlide/ImgSlide';
 
 export const First = () => {
   return (
@@ -8,6 +9,7 @@ export const First = () => {
       <h1>1차 프로젝트 기준으로 기능 구현</h1>
       <MenuTab />
       <SelectBox />
+      <ImgSlide />
     </section>
   );
 };
