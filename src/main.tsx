23104259074e3f726cbf236859a0a52a4e968da9
김명성 <myungsung.kim@overtake.kr>
2024-04-08 11:54:04 +0900

    project start

---
+++ src/main.tsx
@@ -0,0 +1,6 @@
+import ReactDOM from "react-dom/client";
+import Router from "./Router";
+import "@_styles/reset.scss";
+import "@_styles/global.scss";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(<Router />);
---
+++ src/pages/UserForm/UserForm.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const UserForm = () => {
+  return <div>123</div>;
+};
+
+export default UserForm;
